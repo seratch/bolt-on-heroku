561e5af7ccd84e5f94f500b8328ddf37e8297708
Kazuhiro Sera <seratch@gmail.com>
2019-05-29 18:53:26 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Slack Bolt app on Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/seratch/bolt-on-heroku/tree/master)
+
+## Slack ⚡️ Bolt
+
+A framework to build Slack apps, fast.
+
+* https://slack.dev/bolt
+* https://github.com/SlackAPI/bolt
+
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { App } from '@slack/bolt';
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+app.message('hello', ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    say(`Hey there <@${message.user}>\!`);
+});
+(async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running!');
+})();
+

45f16416787b97b3d628975a9514101cee880a6c
Kazuhiro Sera <seratch@gmail.com>
2020-08-14 12:37:07 +0900

    Upgrade the bolt version to the latest

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import { App } from '@slack/bolt';
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 if (process.env.DEBUG) {
-    app.use((args: any) => {
-        console.log(JSON.stringify(args));
-        args.next();
-    });
+  app.use(async (args: any) => {
+    console.log(JSON.stringify(args));
+    return await args.next();
+  });
 }
-app.message('hello', ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    say(`Hey there <@${message.user}>\!`);
+app.message(/hello/, async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
 });
+
 (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
-    console.log('⚡️ Bolt app is running!');
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
 })();
 

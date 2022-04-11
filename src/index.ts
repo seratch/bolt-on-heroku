4c0749ebaf5f95bee23c12a2a8dbd7a4535e10df
Kazuhiro Sera <seratch@gmail.com>
2022-04-11 09:17:13 +0900

    Update the example app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { App } from '@slack/bolt';
+import { App, LogLevel } from '@slack/bolt';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  deferInitialization: true,
+  logLevel: LogLevel.DEBUG,
 });
 if (process.env.DEBUG) {
   app.use(async (args: any) => {
@@ -12,12 +14,20 @@ if (process.env.DEBUG) {
 }
 app.message(/hello/, async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+  if (message.subtype === undefined || message.subtype === 'bot_message') {
+    await say(`Hey there <@${message.user}>!`);
+  }
 });
 
 (async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  try {
+    await app.init();
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running!');
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 })();
 
